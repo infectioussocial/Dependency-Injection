b692caadf8d89217ce55f09ac42943d06e7a32cd
Sande <sande@infectioussocial.com>
2017-01-10 00:34:57 -0500

    first commit

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IDBAccess, SQLDataAccess>();
+            Blogger blogger = container.Resolve<Blogger>();
+        }
+    }
+
+
+    class Blogger
+    {
+        public string Name { get; set; }
+
+
+        IDBAccess _DBAccess;
+        public Blogger(IDBAccess DBAccess)
+        {
+            _DBAccess = DBAccess;
+        }
+    }
+
+
+        interface IDBAccess
+        {
+            string connection { get; set; }
+        }
+
+        class SQLDataAccess : IDBAccess
+        {
+
+            public string _connection;
+            public string connection
+            {
+                get
+                {
+                return "successful connection";
+                }
+                set
+                {
+                    _connection = value;
+                }
+            }
+        }
+}
